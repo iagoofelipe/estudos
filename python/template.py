7c056b57a06eaa89c0e10e442d050b3a6ad273d4
Iago Carvalho <iagoo.feipe@gmail.com>
2023-02-28 16:57:14 -0300

    new file:   python/db.txt 	modified:   python/template.py

--- python/template.py
+++ python/template.py
@@ -1,21 +1,52 @@
-frase = 'O Python é uma linguagem de programação '\
-    'multiparadigma. '\
-    'Python foi criado por Guido van Rossum.'
 
-i = 0
-letra_maior, qtd_letra_maior = str, 0
+db_local = {
+    0 : ['IAGO','123456','iago@gmail.com'],
+}
 
-while i < len(frase):
-    letra_atual = frase[i]
-    qtd_letra_atual = frase.count(letra_atual)
-    i += 1
+def menu():
+    print('Bem-vindo a sua agenda'.center(50))
+    print('','1 - Adicionar contato', '2 - Verificar/consultar contato','', sep='\n')
+    while True:
+        try:
+            opcao = int(input('Escolha a opção desejada: '))
+            if opcao not in (range(1,3)):
+                raise ValueError
+        
+        except ValueError:
+            print('Digite uma opção válida\n')
+            continue
+        else:
+            return opcao
+            break
 
-    if letra_atual == ' ':
+# adicionar contato
+def adicionar():
+    nome = input('Digite o nome: ')
+    telefone = input('Digite o telefone: ')
+    email = input('Digite o e-mail: ')
+
+
+# verificar existentes (consultar)
+def verificar(nome=None):
+    if nome == None:
+        nome = input('Digite o nome: ').upper()
+    
+    if nome in db_local.keys():
+        print('entrou ein')
+
+# atualizar
+
+#---iniciando menu---#
+while True:
+    print('-'*50)
+    menu_resposta = menu()
+    if menu_resposta == 1:
+        verificar()
+
+    opcao_retornar = input('\nDeseja retornar ao menu inicial [S/N]?').lower()
+    
+    if opcao_retornar == 's':
         continue
-
-    if qtd_letra_atual > qtd_letra_maior:
-        qtd_letra_maior = qtd_letra_atual
-        letra_maior = letra_atual
-
-
-print(f'A letra que mais aparece é "{letra_maior}", com {qtd_letra_maior} repetições')
+    
+    else:
+        break
