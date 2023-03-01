2ffd54bffbd02d33e4a545743d0e49c8cae761e1
Iago Carvalho <iagoo.feipe@gmail.com>
2023-03-01 16:43:27 -0300

    modified:   python/template.py 	new file:   teste.py

--- python/template.py
+++ python/template.py
@@ -8,7 +8,7 @@ def menu():
     print('','1 - Adicionar contato', '2 - Verificar/consultar contato','', sep='\n')
     while True:
         try:
-            opcao = int(input('Escolha a opção desejada: '))
+            opcao = int(input('Digite a opção desejada: '))
             if opcao not in (range(1,3)):
                 raise ValueError
         
@@ -19,20 +19,38 @@ def menu():
             return opcao
             break
 
-# adicionar contato
-def adicionar():
-    nome = input('Digite o nome: ')
-    telefone = input('Digite o telefone: ')
-    email = input('Digite o e-mail: ')
-
 
 # verificar existentes (consultar)
 def verificar(nome=None):
     if nome == None:
-        nome = input('Digite o nome: ').upper()
+        
+        nome = input('Digite o parâmetro de pesquisa: ').upper()
     
-    if nome in db_local.keys():
-        print('entrou ein')
+    primeiro = True
+    for i in db_local.values():
+        if nome in i:
+            
+            if primeiro:
+                print(f'{"nome":^20} {"telefone":^20} {"email":^20}')
+                primeiro = False
+                
+            nome = i[0]
+            telefone = i[1]
+            email = i[2]
+            print(f'{nome:>20} {telefone:>20} {email:>20}')
+        
+        else:
+            print('Nenhuma correspondência ')
+
+
+
+# adicionar contato
+def adicionar():
+    nome = input('Digite o nome: ').upper()
+    telefone = input('Digite o telefone: ')
+    email = input('Digite o e-mail: ').upper()
+
+    verificar(nome)
 
 # atualizar
 
@@ -40,13 +58,19 @@ def verificar(nome=None):
 while True:
     print('-'*50)
     menu_resposta = menu()
+
+    print('-'*50)
     if menu_resposta == 1:
+        adicionar()
+    elif menu_resposta == 2:
         verificar()
 
+    print('-'*50)
     opcao_retornar = input('\nDeseja retornar ao menu inicial [S/N]?').lower()
     
     if opcao_retornar == 's':
         continue
     
     else:
+        print('-'*50)
         break
