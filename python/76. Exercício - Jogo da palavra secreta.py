1582231b8f049a1134c21f28ffdf7948d76cc3b8
Iago <iagoo.feipe@gmail.com>
2023-03-03 20:44:06 -0300

    modified:   "Jogo da palavra secreta.py" finalizado

--- python/76. Exercício - Jogo da palavra secreta.py
+++ python/76. Exercício - Jogo da palavra secreta.py
@@ -1,24 +1,49 @@
-palavra_sec = 'perfume'
-palavra_secreta, j = [], 0
+def to_list(string_list=str,to_str=False):
+    """ 
+    retorna list (to_str=False) da palavra[1] e uma lista[0] com "*" para ser alterado
+    retorna str (to_str=True) da palavra[1] e None[0]
+    """
 
-for i in palavra_sec:
-    palavra_secreta[j] = i
-    j += 1
-j = 0
+    retorno, palavra_ast = [],[]
+    
+    if to_str: # utilizar para retornar palavra_ast como str, não retorna nada em retorno
+        palavra_ast, retorno = '', None
+        for i in string_list:
+            palavra_ast += i
+        
+    else: # utilizar para retornar string_list como lista e palavra_ast como lista "*"
+        for i in string_list:
+            retorno.append(i)
+            palavra_ast.append('*')
+        
+    return palavra_ast, retorno
 
-tamanho_palavra = len(palavra_secreta)
-palavra_digitada = '*' * tamanho_palavra
+palavra_secreta = 'algo muito legal' # palavra que será descoberta
+palavra_saida, ps_lista = to_list(palavra_secreta)
+tentativas = 0
 
 while True:
-    letra_digitada = input('Digite uma letra: ')
-    tamanho_letra_digitada = len(letra_digitada)
-
-    if tamanho_letra_digitada > 1:
-        print('Digite apenas uma letra!')
+    contagem = 0
+    letra = input('Digite uma letra: ')
+    
+    if len(letra) != 1: # não aceitar mais de um caractere
+        print('Digite apenas uma letra!\n')
         continue
+    
+    tentativas += 1
+    if letra not in ps_lista: # caso a letra digitada não esteja na lista, evita o resto do código
+        print('Letra não encontrada!', f'\n{tentativas=}\n')  
+    
+    else:
+        for i in ps_lista:
+            if letra == i:
+                palavra_saida[contagem] = i
+            contagem += 1
+        
+        palavra_saida = to_list(palavra_saida,True)[0]
+        print(palavra_saida, f'\n{tentativas=}\n')
+        palavra_saida = to_list(palavra_saida)[1]
 
-    if letra_digitada in palavra_secreta:
-        for i in palavra_secreta:
-            if i == letra_digitada:
-                palavra_secreta[j] = ''
-
+    if '*' not in palavra_saida:
+        print('Fimmmm!!!')
+        break
