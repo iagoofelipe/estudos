924f087c64d51d6ab38d19691a745e518cb05654
Iago Carvalho <iagoo.feipe@gmail.com>
2023-03-09 16:17:03 -0300

    modified:   "90. Exerc\303\255cio - crie uma lista de compras com listas.py" 	new file:   teste.py

--- 90. Exercício - crie uma lista de compras com listas.py
+++ 90. Exercício - crie uma lista de compras com listas.py
@@ -1,36 +1,55 @@
 import os
-lista = []
+lista = ['banana','maçã','tomate']
 #---lista de compras---#
 while True:
     print('Selecione uma opção')
-    opcao = input('[i]nserir [a]pagar [l]istar: ')
+    opcao = input('[i]nserir [a]pagar [l]istar [s]air: ')
 
-    if opcao not in ('i','a','l'):
+    if opcao not in ('i','a','l','s'):
         os.system('cls')
-        print('Opção inválida')
+        print('Opção inválida!\n')
         continue
 
     if opcao == 'i':
         inserir = input('\nValor: ')
         lista.append(inserir)
         os.system('cls')
+        print('Valor inserido com sucesso!\n')
     
     elif opcao == 'a':
-        indices = range(len(lista))
         while True:
             try:
                 indice_apagar = int(input('\nÍndice a ser apagado: '))
-                
-                if indice_apagar not in indices:
-                    os.system('cls')
-                    print('Índice não encontrado!')
+                lista.pop(indice_apagar)
+                os.system('cls')
+                print(f'Valor de índice {indice_apagar} apagado!\n')
                 break
-
-            except:
+                
+            except ValueError:
                 os.system('cls')
                 print('Apenas números inteiros são aceitos!')
                 continue
+            
+            except IndexError:
+                os.system('cls')
+                print(f'Índice {indice_apagar} não encontrado!\n')
+                break
 
     elif opcao == 'l':
-        ...
+        if lista:
+            os.system('cls')
+            print('LISTA'.center(50,'-'))
+            for indice, valor in enumerate(lista):
+                print('-', indice, valor)
+            print('-' * 50, '\n')
+    
+    elif opcao == 's':
+        os.system('cls')
+        print('LISTA'.center(50,'-'))
         
+        for indice, valor in enumerate(lista):
+            print('-', indice, valor)
+        
+        print('-' * 50, '\n')
+        print('Finalizado')
+        break
