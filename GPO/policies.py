d9fdb95d52de2e008999a464e2dd738daa1cc941
Iago <iagoo.feipe@gmail.com>
2023-03-13 17:01:18 -0300

    deleted:    GPO/allComputer.pol 	deleted:    GPO/allUser.pol 	deleted:    GPO/control panel.png 	deleted:    GPO/policies.py 	deleted:    GPO/secpol.cfg 	deleted:    GPO/teste.txt

--- GPO/policies.py
+++
@@ -1,9 +0,0 @@
-import os
-
-abs_path = os.path.abspath('') #caminho do executável
-
-#importando 
-os.system(r"secedit /configure /db c:\windows\security\local.sdb /cfg {local}\secpol.cfg /areas SECURITYPOLICY".format(local=abs_path))
-
-#servidor de internet
-os.system(r"REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers /v 0 /t reg_sz /d a.st1.ntp.br /f")
