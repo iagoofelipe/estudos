9e66e1b7e57f9c5f4b266a2181665014835de99f
Iago Carvalho <iagoo.feipe@gmail.com>
2023-03-13 14:26:25 -0300

    Movendo para local correto

--- GPO/policies.py
+++ GPO/policies.py
@@ -1,4 +1,9 @@
 import os
 
-abs_path = os.path.abspath('')
-os.system(r'secedit /configure /db c:\windows\security\local.sdb /cfg {local}\secpol.cfg /areas SECURITYPOLICY'.format(local=abs_path))
+abs_path = os.path.abspath('') #caminho do executável
+
+#importando 
+os.system(r"secedit /configure /db c:\windows\security\local.sdb /cfg {local}\secpol.cfg /areas SECURITYPOLICY".format(local=abs_path))
+
+#servidor de internet
+os.system(r"REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers /v 0 /t reg_sz /d a.st1.ntp.br /f")
